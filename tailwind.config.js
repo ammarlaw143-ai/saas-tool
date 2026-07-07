dd02998ae966cd7f8b10f883a1b030b724c0d1ba
ammarlaw143-ai <ammarlaw143@gmail.com>
2026-07-07 22:20:03 +0500

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   content: [
-    './src/app/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}'
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {},
